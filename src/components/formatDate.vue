cec46d1d1acf5c78f6387ecbb8b68b18f15f3940
Scott <stsmith@callutheran.edu>
2019-07-03 10:07:08 -0700

    new formatDate component

---
+++ src/components/formatDate.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+	name: 'formatDate',
+	props: ['date'],
+	methods: {
+		formatDate(raceDate) {
+			let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+			let raceUTC = new Date(raceDate);
+
+			return months[raceUTC.getMonth()] + ' ' + raceUTC.getDate() + ', ' + raceUTC.getFullYear();
+		}
+	}
+}
+
+</script>
+
+<template>
+	<time>{{ this.formatDate(this.date) }}</time>
+</template>
