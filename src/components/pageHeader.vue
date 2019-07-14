aad6fd7806bd45d9d79253f48530c0895ffa05a3
Scott Smith <scottsmith24@gmail.com>
2019-07-14 09:04:37 -0700

    new header image

--- src/components/pageHeader.vue
+++ src/components/pageHeader.vue
@@ -12,10 +12,10 @@
 
 	<header class="site-header">
 		<div class="header-contents">
-			<img srcset="@/assets/images/pitwall-stacked-white.svg" class="site-title" />
+			<img srcset="@/assets/images/pitwall-horizontal.svg" class="site-title" />
 
-			<div class="info">
-				<p>Pitwall was created by <a href="https://www.smithscott.net" target="_blank">Scott Smith</a>. Learn more about this project <a href="https://github.com/sts24/pitwall-vue" target="_blank">on the GitHub page.</a></p>
+			<div class="header-info">
+				<p>Created by <a href="https://www.smithscott.net" target="_blank">Scott Smith</a>. Learn more about this project <a href="https://github.com/sts24/pitwall-vue" target="_blank">on the GitHub page.</a></p>
 			</div>
 		</div>
 	</header>
